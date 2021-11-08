cb40c4107cc155aac0ea7bc6d35821cd808d6608
Tomasz Janowski <t.j@duke.edu>
2021-11-08 16:25:26 -0500

    recv is replaced by RX_RING, sending with send()
    It makes me a little bit unsettled is that iperf3 reports some small
    retransmission numbers. Hopefully no bugs, but some delay in userspace
    processing or the traditional per packet send() is causing delays.

--- main.cc
+++ main.cc
@@ -11,6 +11,8 @@
 #include <poll.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/user.h>
+#include <sys/mman.h>
 using namespace std;
 const int LEN=16384;
 //unsigned char buffer[LEN];
@@ -55,6 +57,8 @@ struct thread_data {
 int *fds;
 int source;
 char **argv;
+unsigned char ** mmaps_rx;
+unsigned int mmap_size;
 };
 void * thread_forwarder(void * y) {
   sockaddr_ll sckbind;
@@ -64,39 +68,70 @@ void * thread_forwarder(void * y) {
   int *fds=reinterpret_cast<thread_data*>(y)->fds;
   int source=reinterpret_cast<thread_data*>(y)->source;
   char **argv=reinterpret_cast<thread_data*>(y)->argv;
+  unsigned char **mmaps_rx=reinterpret_cast<thread_data*>(y)->mmaps_rx;
+  unsigned int size=reinterpret_cast<thread_data*>(y)->mmap_size;
+  unsigned int max_fr=size/2048;
   cout << "source " << source << endl;
+//if (source) sleep(360000);
+  pollfd pl;
+  pl.fd=fds[source];
+  pl.events=POLLIN|POLLRDNORM|POLLERR;
 //  receive and print packet
+  unsigned int circ_rx=0;
+  sockaddr_ll *addr;
+  bool oneframe=true;
   for (;;) {
-    x=recvfrom(fds[source],buffer,LEN,0,reinterpret_cast<sockaddr*>(&sckbind),&socklen);
-    if (x<0) {
-      perror("recv");
-      throw "Dupa 2";
-      }
-    if (sckbind.sll_pkttype==PACKET_OUTGOING) continue;
-//  cout << "From " << argv[source+1] << endl;
-//  print_hex(buffer,x);
-//  if (checksum(buffer,x,&checksum_found,&checksum_calculated)) cout << hex << checksum_found << " " << checksum_calculated << endl;
-    if (x>1514) { // 1500 + MAC + MAC + ethtype
-      cout << x << endl;
-      throw "Packet bigger than 1500 bytes";
-      }
-    x=send(fds[source!=1],buffer,x,0);
-    if (x<0) {
-      perror("send");
-      cout << "From " << argv[source+1] << endl;
-      print_hex(buffer,x);
-      throw "Dupa s";
+    if (!oneframe) cout << "Did not get any frame" << endl;
+    pl.revents=0;
+    poll(&pl,1,-1);
+    oneframe=false;
+//  cout << "Have poll" << endl;
+    for (;;circ_rx=(circ_rx+1)%max_fr) {
+      unsigned char *frame=mmaps_rx[source]+circ_rx*2048;
+      tpacket_hdr *hdr=reinterpret_cast<tpacket_hdr*>(frame);
+      if ((hdr->tp_status&TP_STATUS_USER)==TP_STATUS_USER) {
+        oneframe=true;
+//      if (hdr->tp_status&TP_STATUS_LOSING==TP_STATUS_LOSING) cout << "Losing packets" << endl;
+//      cout << "Have packet " << circ_rx << endl;
+//      cout << "From " << argv[source+1] << endl;
+//      print_hex(frame+hdr->tp_mac,hdr->tp_snaplen);
+        if (hdr->tp_snaplen!=hdr->tp_len) throw "truncation?";
+        addr=reinterpret_cast<sockaddr_ll*>(frame+TPACKET_ALIGN(sizeof(tpacket_hdr)));
+        if (hdr->tp_snaplen>1514) { // 1500 + MAC + MAC + ethtype
+          cout << hdr->tp_snaplen << endl;
+          throw "Packet bigger than 1500 bytes";
+          }
+        if (addr->sll_pkttype==PACKET_OUTGOING) {
+          hdr->tp_status=TP_STATUS_KERNEL;
+          continue;
+          }
+        x=send(fds[source!=1],frame+hdr->tp_mac,hdr->tp_snaplen,0);
+        if (x<0) {
+          perror("send");
+          cout << "From " << argv[source+1] << endl;
+          print_hex(buffer,x);
+          throw "Dupa s";
+          }
+        hdr->tp_status=TP_STATUS_KERNEL;
+        }
+      else break;
       }
     }
   return 0;
   }
 int main(int argc , char * argv[]) try {
   pthread_t thr_id[2];
-  int ret,fd[2];
+  int ret,fd[2]={};
+  unsigned char * space_rx[2]={};
   if (argc<3) throw "arguments are missing";
   sockaddr_ll sckbind;
+  tpacket_req ring_parameters={};
+  ring_parameters.tp_block_size=PAGE_SIZE*1024*128;
+  ring_parameters.tp_block_nr=4;
+  ring_parameters.tp_frame_size=2048;
+  ring_parameters.tp_frame_nr=ring_parameters.tp_block_size/ring_parameters.tp_frame_size*ring_parameters.tp_block_nr;
 
-// LOOP:
+// SETUP LOOP:
 
   struct ifreq netdevice;
   for (int k=0;k<2;++k) {
@@ -110,9 +145,12 @@ int main(int argc , char * argv[]) try {
     ret=ioctl(fd[k],SIOCGIFINDEX,&netdevice);
     perror("ioctl");
     if (ret<0) throw "Dupa 3";
-    sckbind.sll_family=AF_PACKET;
-    sckbind.sll_protocol=0;
-    sckbind.sll_ifindex=netdevice.ifr_ifindex;
+    ret=setsockopt(fd[k],SOL_PACKET,PACKET_RX_RING,&ring_parameters,sizeof(ring_parameters));
+    if (ret<0) {
+      perror("setsockopt");
+      throw "Dupa 10";
+      }
+/*
     long mem=16777216;
     socklen_t lmem=sizeof(mem);
     ret=setsockopt(fd[k],SOL_SOCKET,SO_RCVBUFFORCE,&mem,lmem);
@@ -127,6 +165,16 @@ int main(int argc , char * argv[]) try {
       throw "Dupa 9";
       }
     cout << "Socket buffer: " << mem << " " << lmem << endl;
+*/
+    space_rx[k]=reinterpret_cast<unsigned char*>(mmap(NULL, ring_parameters.tp_block_size*ring_parameters.tp_block_nr, PROT_READ|PROT_WRITE, MAP_SHARED, fd[k], 0));
+    if (space_rx[k]==MAP_FAILED) {
+      perror("mmap");
+      throw "Dupa 11";
+      }
+//
+    sckbind.sll_family=AF_PACKET;
+    sckbind.sll_protocol=0;
+    sckbind.sll_ifindex=netdevice.ifr_ifindex;
     ret=bind(fd[k],reinterpret_cast<sockaddr*>(&sckbind),sizeof(sckbind));
     if (ret<0) throw "Dupa 4";
     }
@@ -136,6 +184,8 @@ struct thread_data thr_dat[2];
     thr_dat[i].fds=fd;
     thr_dat[i].source=i;
     thr_dat[i].argv=argv;
+    thr_dat[i].mmaps_rx=space_rx;
+    thr_dat[i].mmap_size=ring_parameters.tp_block_size*ring_parameters.tp_block_nr;
     pthread_create(thr_id+i,NULL,thread_forwarder,thr_dat+i);
     }
   for (int i=0;i<2;++i) {
