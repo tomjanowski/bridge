dc85bd54d32db2945ab0916423306b004e98e245
Tomasz Janowski <t.j@duke.edu>
2021-11-10 10:52:16 -0500

    Buffer size not if properly rounded

--- main.cc
+++ main.cc
@@ -99,8 +99,9 @@ int main(int argc , char * argv[]) try {
 //! cout << "Set " << argv[k+1] << " to PROMISCUOUS mode" << endl;
     long mem=ring_parameters_tx.tp_block_size*ring_parameters_tx.tp_block_nr/
              ring_parameters_tx.tp_frame_size*1514/10;
-    mem=(mem+PAGE_SIZE)&(~PAGE_SIZE);
+    mem=((mem+PAGE_SIZE-1)&(~(PAGE_SIZE-1)));
     mem=((mem<MEGABYTE)?MEGABYTE:mem);
+    cout << "Initial send buffer requested " << mem << endl;
     socklen_t lmem=sizeof(mem);
     ret=setsockopt(fd[k],SOL_SOCKET,SO_SNDBUFFORCE,&mem,lmem);
     if (ret<0) {
